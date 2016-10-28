24453fd872032def79f764c5a22005c8cddee595
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2016-10-29 02:19:00 +0800

    deleted test data

--- 2d-array.js
+++ 2d-array.js
@@ -1,11 +1,3 @@
-var arr =   [ [ 1, 1, 1, 0, 0, 0 ],
-            [ 0, 1, 0, 0, 0, 0 ],
-            [ 1, 1, 1, 0, 0, 0 ],
-            [ 0, 9, 2, -4, -4, 0 ],
-            [ 0, 0, 0, -2, 0, 0 ],
-            [ 0, 0, -1, -2, -4, 0 ] ];
-
-
 var cols = arr.length;
 var rows = arr[0].length;
 var tempSum = -1000000;  //sake of stupidty. 
@@ -13,13 +5,6 @@ var tempSum = -1000000;  //sake of stupidty.
 for (var i = 0; i < cols-2; i++ ){
     for (var j = 0; j < rows-2; j++){
         var item = arr[i][j];
-        // var hourglass =  [
-        //                     arr[i][j] ,    arr[i][j+1]   ,  arr[i][j+2] ,
-        //                   /*arr[i+1][j] */ arr[i+1][j+1] , /*arr[i+1][j+2]*/
-        //                     arr[i+2][j] ,  arr[i+2][j+1] ,  arr[i+2][j+2]
-        //                  ];
-
-
         var tempSumHourGlass = arr[i][j] + arr[i][j+1] + arr[i][j+2]  + arr[i+1][j+1]  + arr[i+2][j] +  arr[i+2][j+1] + arr[i+2][j+2]
         if (tempSumHourGlass >= tempSum ){
             sum = tempSumHourGlass;
